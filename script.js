48020b8bcfbf623616c0c1bd9c6fcad251e4bf4b
leilan2027-byte <leilan2027@gmail.com>
2026-03-10 18:37:58 +0000

    3456

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log("Script started");
+
+function flipCard(cardId) {
+    // Get the specific card (start with card1)
+    let card = document.getElementById(cardId);
+    card.style.color = "white";
+    card.style.backgroundColor = "cornflowerblue";
+}
+
+flipCard
